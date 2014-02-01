6137ccf8424caa971a58dbb4bfca4518e131db67
Pengyu Chen <cpy4ever@gmail.com>
2014-02-01 21:32:21 +0800

    Create template.cpp
    NULL

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <list>
+
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+#define LEN(s) ((int)(s).length())
+#define SZ(v) ((int)(v).size())
+#define INF32 (1 << 30)
+#define INF64 (1LL << 60)
+#define PB push_back
+#define CLR(v) ((v).clear())
+#define ALL(v) (v).begin(), (v).end()
+#define ZERO(v) memset(v, 0, sizeof(v))
+
+typedef long long ll;

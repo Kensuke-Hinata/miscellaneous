9a85bc470bd394590621b4ad437683a5ebe5cc13
Kensuke Hinata <cpy4ever@gmail.com>
2016-06-08 21:03:38 +0800

    add hackerrank

---
+++ Hackerrank/fp/List Length.hs
@@ -0,0 +1,4 @@
+len :: [a] -> Int
+len [] = 0
+len (x : xs) = 1 + len xs
+
---
+++ Hackerrank/fp/Update List.hs
@@ -0,0 +1,7 @@
+f [] = []
+f (x : xs) = if x > 0 then x : f xs else -x : f xs
+
+-- This section handles the Input/Output and can be used as it is. Do not modify it.
+main = do
+   inputdata <- getContents
+   mapM_ putStrLn $ map show $ f $ map (read :: String -> Int) $ lines inputdata

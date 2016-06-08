46ddba168dea2a8f827a8c3f4a7c50911b0aecd4
Kensuke Hinata <cpy4ever@gmail.com>
2016-06-08 20:57:32 +0800

    refactor directory

--- Hackerrank/fp/Hello World N Times.hs
+++
@@ -1,8 +0,0 @@
-hello_worlds :: Int -> [Char]
-hello_worlds n
-	| n <= 0 = ""
-	| otherwise = "Hello World\n" ++ hello_worlds (n - 1)
-
-main = do
-	n <- readLn :: IO Int
-	putStrLn (hello_worlds n)

6ae8da7d957e87f8fadfaaa818f7c3adb9d8f4f1
Kensuke-Hinata <cpy4ever@gmail.com>
2020-05-26 21:37:16 +0800

    update ProjectEuler/612

--- ProjectEuler/612/main.d
+++ ProjectEuler/612/main.d
@@ -8,10 +8,7 @@ int saiki(int idx, int n, int pmask, int qmask, int flag, int[][][][] dp, int mo
 {
     if (idx == n)
     {
-        if (flag == 0 || pmask == 1 || (pmask & qmask) == 0)
-        {
-            return 0;
-        }
+        if (flag == 0 || pmask == 1 || (pmask & qmask) == 0) return 0;
         //if (p in dup && dup[p] == q) writeln("damn it");
         //if (p >= q || p >= pow(cast(long)10, n) || q >= pow(cast(long)10, n) || p == 0 || q == 0)
         //{
@@ -25,10 +22,7 @@ int saiki(int idx, int n, int pmask, int qmask, int flag, int[][][][] dp, int mo
         ++ cnt;
         return 1;
     }
-    if (dp[idx][pmask][qmask][flag] != -1)
-    {
-        return dp[idx][pmask][qmask][flag];
-    }
+    if (dp[idx][pmask][qmask][flag] != -1) return dp[idx][pmask][qmask][flag];
     if (flag == 1 && pmask > 1 && (pmask & qmask) != 0)
     {
         auto choices = pow(cast(long)10, n - idx) % mod;
@@ -39,33 +33,18 @@ int saiki(int idx, int n, int pmask, int qmask, int flag, int[][][][] dp, int mo
     foreach (i; 0 .. 10)
     {
         auto qlower = 0;
-        if (flag == 0)
-        {
-            qlower = i;
-        }
+        if (flag == 0) qlower = i;
         auto pNextMask = pmask;
-        if (pmask > 1 || i > 0)
-        {
-            pNextMask |= 1 << i;
-        }
+        if (pmask > 1 || i > 0) pNextMask |= 1 << i;
         foreach (j; qlower .. 10)
         {
             auto nextFlag = flag;
-            if (i < j)
-            {
-                nextFlag = 1;
-            }
+            if (i < j) nextFlag = 1;
             auto qNextMask = qmask;
-            if (qmask > 1 || j > 0)
-            {
-                qNextMask |= 1 << j;
-            }
+            if (qmask > 1 || j > 0) qNextMask |= 1 << j;
             auto ret = saiki(idx + 1, n, pNextMask, qNextMask, nextFlag, dp, mod, p * 10 + i, q * 10 + j, cnt);
             res += ret;
-            if (res >= mod)
-            {
-                res -= mod;
-            }
+            if (res >= mod) res -= mod;
         }
     }
     dp[idx][pmask][qmask][flag] = res;
@@ -76,15 +55,9 @@ void solve()
 {
     auto n = 18;
     auto dp = new int[][][][](n, 1024, 1024, 2);
-    foreach (i; 0 .. n)
+    foreach (i; 0 .. n) foreach (j; 0 .. 1024)
     {
-        foreach (j; 0 .. 1024)
-        {
-            foreach (k; 0 .. 1024)
-            {
-                fill(dp[i][j][k], -1);
-            }
-        }
+        foreach (k; 0 .. 1024) fill(dp[i][j][k], -1);
     }
     long cnt = 0;
     auto res = saiki(0, n, 0, 0, 0, dp, 1000267129, 0, 0, cnt);
@@ -99,22 +72,13 @@ void test()
     {
         auto pmask = 0;
         auto s = to!string(i);
-        foreach (c; s)
-        {
-            pmask |= 1 << (c - '0');
-        }
+        foreach (c; s) pmask |= 1 << (c - '0');
         foreach (j; i + 1.. 100)
         {
             auto qmask = 0;
             s = to!string(j);
-            foreach (c; s)
-            {
-                qmask |= 1 << (c - '0');
-            }
-            if (pmask & qmask)
-            {
-                ++ res;
-            }
+            foreach (c; s) qmask |= 1 << (c - '0');
+            if (pmask & qmask) ++ res;
         }
     }
     writeln(res);

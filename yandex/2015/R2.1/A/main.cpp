1013864a935ec9f784f681b2778828e56c087906
Kensuke-Hinata <cpy4ever@gmail.com>
2020-10-06 16:47:09 +0800

    update yandex/2015/R2.1/A

--- yandex/2015/R2.1/A/main.cpp
+++ yandex/2015/R2.1/A/main.cpp
@@ -9,11 +9,11 @@ typedef long long LL;
 int solve(vector<int>& h, int m) {
     static const int mod = 1000000007;
     auto n = SZ(h);
-    vector<int> dp(n + 1), c(n + 1);
+    vector<int> c(n + 1);
     vector<LL> s(n + 1);
-    fill(dp.begin(), dp.end(), 0);
-    dp[n] = c[n] = 1;
+    c[n] = 1;
     s[n] = 0;
+    auto dp = 0;
     for (int i = n - 1; i >= 0; -- i) {
         s[i] = s[i + 1] + h[i];
         auto left = i, right = n - 1;
@@ -28,15 +28,15 @@ int solve(vector<int>& h, int m) {
             }
         }
         if (t != -1) {
-            dp[i] = c[i + 1];
+            dp = c[i + 1];
             if (t + 2 <= n) {
-                dp[i] -= c[t + 2];
-                if (dp[i] < 0) dp[i] += mod;
+                dp -= c[t + 2];
+                if (dp < 0) dp += mod;
             }
         }
-        c[i] = (c[i + 1] + dp[i]) % mod;
+        c[i] = (c[i + 1] + dp) % mod;
     }
-    return dp[0];
+    return dp;
 }
 
 int main(int argc, char** argv) {

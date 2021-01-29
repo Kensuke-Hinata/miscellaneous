fa9bbd0aa9afeb879a5e46c7fd0b5079ceee2086
Kensuke-Hinata <cpy4ever@gmail.com>
2021-01-29 16:59:47 +0800

    update TopCoder/SRM/437/TheInteger

--- TopCoder/SRM/437/TheInteger/Main.cs
+++ TopCoder/SRM/437/TheInteger/Main.cs
@@ -6,7 +6,7 @@ public class TheInteger
 {
     static long Inf = 9223372036854775807;
 
-    List<int> ToBits(long num)
+    List<int> GetDigits(long num)
     {
         List<int> res = new List<int>();
         while (num > 0)
@@ -18,24 +18,24 @@ public class TheInteger
         return res;
     }
 
-    long Recur(int idx, int mask, int flag, int k, long[,,] dp, List<int> b, long[,] mul)
+    long Recur(int idx, int mask, int flag, int k, long[,,] dp, List<int> ds, long[,] mul)
     {
         if (dp[idx, mask, flag] != -1) return dp[idx, mask, flag];
-        if (idx == b.Count)
+        if (idx == ds.Count)
         {
             dp[idx, mask, flag] = (k == 0) ? 0 : Inf;
             return dp[idx, mask, flag];
         }
         long res = Inf;
-        int start = (flag == 0) ? b[idx] : 0; 
+        int start = (flag == 0) ? ds[idx] : 0; 
         for (int i = start; i <= 9; ++ i)
         {
-            int nflag = (i > b[idx]) ? 1 : flag;
+            int nflag = (i > ds[idx]) ? 1 : flag;
             int nk = ((mask & (1 << i)) != 0) ? k : (k - 1);
-            long ret = Recur(idx + 1, mask | (1 << i), nflag, nk, dp, b, mul);
+            long ret = Recur(idx + 1, mask | (1 << i), nflag, nk, dp, ds, mul);
             if (ret != Inf)
             {
-                res = mul[i, b.Count - idx] + ret;
+                res = mul[i, ds.Count - idx] + ret;
                 break;
             }
         }
@@ -48,20 +48,19 @@ public class TheInteger
         long[,] mul = new long[10, 20];
         for (int i = 0; i <= 9; ++ i)
         {
-            mul[i, 0] = 1;
             mul[i, 1] = i;
             for (int j = 2; j <= 19; ++ j) mul[i, j] = mul[i, j - 1] * 10;
         }
-        List<int> b = ToBits(n);
-        long[,,] dp = new long[b.Count + 1, 1024, 2];
-        for (int i = 0; i < b.Count + 1; ++ i)
+        List<int> ds = GetDigits(n);
+        long[,,] dp = new long[ds.Count + 1, 1024, 2];
+        for (int i = 0; i < ds.Count + 1; ++ i)
         {
             for (int j = 0; j < 1024; ++ j) dp[i, j, 0] = dp[i, j, 1] = -1;
         }
-        long ret = Recur(0, 0, 0, k, dp, b, mul);
+        long ret = Recur(0, 0, 0, k, dp, ds, mul);
         if (ret != Inf) return ret;
         long res = 10;
-        for (int i = 0; i < Math.Max(b.Count + 1, k) - k; ++ i) res *= 10;
+        for (int i = 0; i < Math.Max(ds.Count + 1, k) - k; ++ i) res *= 10;
         for (int d = 2; d < k; ++ d) res = res * 10 + d;
         return res;
     }

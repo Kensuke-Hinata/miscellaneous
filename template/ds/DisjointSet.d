17864d95d3c31025de829d9d8b555e1ffea39fa7
Kensuke-Hinata <cpy4ever@gmail.com>
2021-09-10 15:53:59 +0800

    update template/ds/DisjointSet.d

--- template/ds/DisjointSet.d
+++ template/ds/DisjointSet.d
@@ -3,36 +3,34 @@ import std.algorithm;
 
 class DisjointSet
 {
-    protected:
-        int[] parent;
-        int[] height;
+    protected int[] parent;
+    protected int[] height;
 
-    public:
-        this(int n)
+    this(int n)
+    {
+        parent = new int[n];
+        height = new int[n];
+        foreach (i; 0 .. n)
         {
-            parent = new int[n];
-            height = new int[n];
-            foreach (i; 0 .. n)
-            {
-                parent[i] = i;
-                height[i] = 1;
-            }
+            parent[i] = i;
+            height[i] = 1;
         }
+    }
 
-        int find(int n)
-        {
-            if (parent[n] == n) return n;
-            parent[n] = find(parent[n]);
-            return parent[n];
-        }
+    int find(int n)
+    {
+        if (parent[n] == n) return n;
+        parent[n] = find(parent[n]);
+        return parent[n];
+    }
 
-        void join(int n, int m)
-        {
-            int pn = find(n), pm = find(m);
-            if (height[pn] > height[pm]) swap(pn, pm);
-            parent[pn] = pm;
-            if (height[pn] == height[pm]) ++ height[pm];
-        }
+    void join(int n, int m)
+    {
+        int pn = find(n), pm = find(m);
+        if (height[pn] > height[pm]) swap(pn, pm);
+        parent[pn] = pm;
+        if (height[pn] == height[pm]) ++ height[pm];
+    }
 };
 
 int main(string[] args)

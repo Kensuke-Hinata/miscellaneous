140161247f142ae1893a84071dd92782ab4f30a0
Kensuke-Hinata <cpy4ever@gmail.com>
2021-09-12 02:29:01 +0800

    update template/ds/Hash.d

--- template/ds/Hash.d
+++ template/ds/Hash.d
@@ -2,53 +2,51 @@ import std.stdio, std.string, std.conv;
 
 class Hash
 {
-    public:
-        this(int n)
-        {
-            len = n << 3;
-            h = new int[][len];   // list for the same value v
-            f = new bool[len];    // occupation flag
-            v = new long[len];    // value
-        }
+    protected int len;
+    protected long[] v;
+    protected bool[] f;
+    protected int[][] h;
 
-        int getPosition(long n)
-        {
-            auto s = to!string(n);
-            int pos = calc(s);
-            while (f[pos] == true && v[pos] != n)
-            {
-                ++ pos;
-                if (pos == len) pos = 0;
-            }
-            return pos;
-        }
+    this(int n)
+    {
+        len = n << 3;
+        h = new int[][len];   // list for the same value v
+        f = new bool[len];    // occupation flag
+        v = new long[len];    // value
+    }
 
-        void insert(long n, int idx)
+    int getPosition(long n)
+    {
+        auto s = to!string(n);
+        int pos = calc(s);
+        while (f[pos] == true && v[pos] != n)
         {
-            auto pos = getPosition(n);
-            v[pos] = n;
-            f[pos] = true;
-            h[pos] ~= idx;
+            ++ pos;
+            if (pos == len) pos = 0;
         }
+        return pos;
+    }
 
-        int[] find(long n)
-        {
-            auto pos = getPosition(n);
-            return h[pos];
-        }
+    void insert(long n, int idx)
+    {
+        auto pos = getPosition(n);
+        v[pos] = n;
+        f[pos] = true;
+        h[pos] ~= idx;
+    }
 
-        int calc(string s)
-        {
-            long hash = 0;
-            foreach (i, val; s) hash = (hash << 4) ^ (hash >> 28) ^ val;
-            return (hash % len);
-        }
+    int[] find(long n)
+    {
+        auto pos = getPosition(n);
+        return h[pos];
+    }
 
-    protected:
-        int len;
-        long[] v;
-        bool[] f;
-        int[][] h;
+    int calc(string s)
+    {
+        long hash = 0;
+        foreach (i, val; s) hash = (hash << 4) ^ (hash >> 28) ^ val;
+        return (hash % len);
+    }
 };
 
 int main(string[] args)

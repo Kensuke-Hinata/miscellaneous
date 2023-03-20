d1f617b226eb64c18b3c0d3181db2c2fe4ae8785
Pengyu Chen <pengyu.chen@fuseignited.com>
2023-03-20 18:38:46 +0800

    update vkcup/2012/qual1

--- vkcup/2012/qual1/D/main.c
+++ vkcup/2012/qual1/D/main.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
-#define INF 1 << 30
+#define INF (1 << 30)
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int t[20000];
 
 void func(int n) {
-  int m = 0, i, j, k, c, ans = -INF, s;
-  for (i = 1; i <= n; i ++) {
-    if (n % i != 0) {
-      continue;
+    int m = 0, i, j, k, c, ans = -INF, s;
+    for (i = 1; i <= n; i ++) {
+        if (n % i != 0) {
+            continue;
+        }
+        for (j = 0; j < n && j < i; j ++) {
+            s = c = 0;
+            for (k = j; k < n; k += i) {
+                s += t[k];
+                c ++;
+            }
+            if (c >= 3) {
+                ans = MAX(ans, s);
+            }
+        }
     }
-    for (j = 0; j < n && j < i; j ++) {
-      s = c = 0;
-      for (k = j; k < n; k += i) {
-        s += t[k];
-        c ++;
-      }
-      if (c >= 3) {
-        ans = MAX(ans, s);
-      }
-    }
-  }
-  printf("%d\n", ans);
+    printf("%d\n", ans);
 }
 
 int main() {
-  int n, i;
-  while (scanf("%d", &n) == 1) {
-    for (i = 0; i < n; i ++) {
-      scanf("%d", &t[i]);
+    int n, i;
+    while (scanf("%d", &n) == 1) {
+        for (i = 0; i < n; i ++) {
+            scanf("%d", &t[i]);
+        }
+        func(n);
     }
-    func(n);
-  }
-  return 0;
+    return 0;
 }
